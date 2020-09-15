30ac38134f0bb10dee9a7e711994f6cc8dd262e5
Daniel O'Brien <daobrien@miners.utep.edu>
2020-09-14 18:03:40 -0600

    Added notification php sample

--- views/setupContentView.php
+++ views/setupContentView.php
@@ -2,9 +2,22 @@
 <html lang="en">
 <head>
     <?php include '../templates/header.php';?>
+	<?php include 'notificationSample.php';?>
 </head>
 <body>
+<?php
+	if(array_key_exists('button1', $_POST)) { 
+        button1(); 
+    }
+	function button1() {
+		echo "<script type='text/javascript'>openNotification();</script>";
+	}
+?>
     <!--Write Your code here-->
+	<form method="post"> 
+        <input type="submit" name="button1"
+                class="button" value="Button1" />
+	</form>
 </body>
 <footer class="footer">
     <?php include '../templates/footer.php';?>
