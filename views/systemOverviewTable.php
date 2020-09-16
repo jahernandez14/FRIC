a6f40d8779889e1de097d0394a9b050de0df5844
WilliamBasquez <webasquez@outlook.com>
2020-09-16 10:51:12 -0600

    New file created to insert System Overview table
    Since we need this overview table in more than one screen, it makes sense to separate it from the System screen and have it being its own file

---
+++ views/systemOverviewTable.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<body>
+  <div class="col-12">
+    <table class="table table-light table-striped">
+      <thead class="text-center">
+        <tr>
+          <th scope="col"><input type="checkbox"></th>
+          <th scope="col">System &nbsp;<button>&uarr;</button><button>&darr;</button></th>
+          <th scope="col">No. of Tasks &nbsp;<button>&uarr;</button><button>&darr;</button></th>
+          <th scope="col">No. of Findings &nbsp;<button>&uarr;</button><button>&darr;</button></th>
+          <th scope="col">Progress &nbsp;<button>&uarr;</button><button>&darr;</button></th>
+        </tr>
+      </thead>
+      <tbody class="text-center">
+        <tr>
+          <th scope="col"><input type="checkbox"></th>
+            <td>System 1</td>
+            <td>2</td>
+            <td>5</td>
+            <td>In Progress</td>
+          </tr>
+          <tr>
+            <th scope="col"><input type="checkbox"></th>
+              <td>System 2</td>
+              <td>3</td>
+              <td>4</td>
+              <td>Assigned</td>
+            </tr>
+            <tr>
+              <th scope="col"><input type="checkbox"></th>
+              <td>System 3</td>
+              <td>5</td>
+              <td>2</td>
+              <td>In Progress</td>
+            </tr>
+            <tr>
+         </tbody>
+      </table>
+   </div>
+</body>
+</html>
