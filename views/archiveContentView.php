4ab0778d57c41affe45004b079b69a898360d50b
WilliamBasquez <webasquez@outlook.com>
2020-09-16 10:52:08 -0600

    Files modified to include systemOverviewTable file
    The files include the system overview table, still need to add 3 more tables

--- views/archiveContentView.php
+++ views/archiveContentView.php
@@ -7,13 +7,35 @@
     <div class="container-fluid">
         <div class="row fluid-col">
             <div class="col-2" style = "background-color:#202020">
-            <?php include '../templates/eventTree.php';?>
+                <?php include '../templates/eventTree.php';?>
             </div>
             <div class="col-8">
-            <!--Code Goes Here-->
+                <!--Code Goes Here-->
+                <form>
+                  <h2 class="text-center">Archived Tasks</h2>
+                  <div class="row">
+                    <h5>[INSERT TASK OVERVIEW TABLE]</h5>
+                  </div>
+                  <button type="button" class="btn btn-light">Restore</button><br><br>
+                  <h2 class="text-center">Archived Subtasks</h2>
+                  <div class="row">
+                    <h5>[INSERT SUBTASK OVERVIEW TABLE]</h5>
+                  </div>
+                  <button type="button" class="btn btn-light">Restore</button><br><br>
+                  <h2 class="text-center">Archived Findings</h2>
+                  <div class="row">
+                    <h5>[INSERT FINDINGS OVERVIEW TABLE]</h5>
+                  </div>
+                  <button type="button" class="btn btn-light">Restore</button><br><br>
+                  <h2 class="text-center">Archived Systems</h2>
+                  <div class="row">
+                    <?php include '../views/systemOverviewTable.php';?>
+                  </div>
+                  <button type="button" class="btn btn-light">Restore</button><br><br>
+                </form>
             </div>
             <div class="col-2"  style = "background-color:#202020">
-            <?php include '../templates/search.php';?>
+                <?php include '../templates/search.php';?>
             </div>
         </div>
     </div>
--- views/systemContentView.php
+++ views/systemContentView.php
@@ -11,45 +11,11 @@
                 </div>
                 <div class="col-8">
                     <!--Code goes here-->
-                    <!--Code copied from eventContentView-->
                     <h2 class = "text-center">System Overview Table</h2>
-                    <table class="table table-light table-striped">
-                        <!--Add icon-->
-                        <button type="button" class="btn btn-light">+</button>
-                        <thead>
-                            <tr>
-                                <th scope="col"><input type="checkbox"></th>
-                                <th scope="col">System &nbsp;<button>&uarr;</button><button>&darr;</button></th>
-                                <th scope="col">No. of Tasks &nbsp;<button>&uarr;</button><button>&darr;</button></th>
-                                <th scope="col">No. of Findings &nbsp;<button>&uarr;</button><button>&darr;</button></th>
-                                <th scope="col">Progress &nbsp;<button>&uarr;</button><button>&darr;</button></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="col"><input type="checkbox"></th>
-                                <td>System 1</td>
-                                <td>2</td>
-                                <td>5</td>
-                                <td>In Progress</td>
-                            </tr>
-                            <tr>
-                                <th scope="col"><input type="checkbox"></th>
-                                <td>System 2</td>
-                                <td>3</td>
-                                <td>4</td>
-                                <td>Assigned</td>
-                            </tr>
-                            <tr>
-                                <th scope="col"><input type="checkbox"></th>
-                                <td>System 3</td>
-                                <td>5</td>
-                                <td>2</td>
-                                <td>In Progress</td>
-                            </tr>
-                            <tr>
-                        </tbody>
-                    </table>
+                    <button type="button" class="btn btn-light">+</button>
+                    <div class="row">
+                      <?php include '../views/systemOverviewTable.php';?>
+                    </div>
                     <!--Missing 'help' button/icon-->
                     <h2 class = "text-center">System Detailed View</h2>
                     <h4>System Information</h4>
