0f7d98cbc8c4b34a4eb0918760a78caf778a4d59
Julio Hernandez <jahernandez14@miners.utep.edu>
2020-09-30 18:29:39 -0600

    event edited

--- controller/eventController.php
+++ controller/eventController.php
@@ -9,19 +9,19 @@
 
     function readEvent($eventName){
         $db = new Database();
-        $eventArray = $db->getDocument('$eventName','FRIC_Database.Event');
+        $eventArray = $db->getDocument($eventName,'FRIC_Database.Event');
         return $eventArray;
     }
 
     function createEvent($eventName, $eventDescription, $eventType, $eventVersion, $assessmentDate, $organizationName, $securityClassifcation, $eventClassification, $declassificationDate, $customerName, $archiveStatus, $eventTeam, $numberOfFindings, $numberOfSystems, $progress){
         $db = new Database();
-        new Event($db, $eventName, $eventDescription, $eventType, $eventVersion, $assessmentDate, $organizationName, $securityClassifcation, $eventClassification, $declassificationDate, $customerName, $archiveStatus, $eventTeam, $numberOfFindings, $numberOfSystems, $progress);
+        new Event($db, $eventName, $eventDescription, $eventType, $eventVersion, $assessmentDate, $organizationName, $securityClassifcation, $eventClassification, $declassificationDate, $customerName, $archiveStatus, $eventTeam, '', $numberOfFindings, $numberOfSystems, $progress);
     }
 
     function editEvent($eventName, $eventDescription, $eventType, $eventVersion, $assessmentDate, $organizationName, $securityClassifcation, $eventClassification, $declassificationDate, $customerName, $archiveStatus, $eventTeam, $numberOfFindings, $numberOfSystems, $progress){
         $db = new Database();
-        $db->editDocument('$eventName','FRIC_Database.Event');
-        new Event($db, $eventName, $eventDescription, $eventType, $eventVersion, $assessmentDate, $organizationName, $securityClassifcation, $eventClassification, $declassificationDate, $customerName, $archiveStatus, $eventTeam, $numberOfFindings, $numberOfSystems, $progress);
+        $db->editDocument($eventName,'FRIC_Database.Event');
+        new Event($db, $eventName, $eventDescription, $eventType, $eventVersion, $assessmentDate, $organizationName, $securityClassifcation, $eventClassification, $declassificationDate, $customerName, $archiveStatus, $eventTeam, '', $numberOfFindings, $numberOfSystems, $progress);
     }
 
     function archiveEvent($eventName){
