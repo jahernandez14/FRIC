947fd3b516b7b2958b317aecf35c56e577f9b6fa
WilliamBasquez <webasquez@outlook.com>
2020-10-02 23:21:39 -0600

    Added getting started page
    This page will have information regarding MongoDB, server info, and other links (not needed)

---
+++ view/views/gettingStarted.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <a id="startPage"></a>
+    <?php include '../templates/header.php';?>
+</head>
+
+<body>
+
+    <div class="container-fluid content">
+        <div class="row">
+            <div id="eventTree" class="dm-popout" style="background-color:#202020">
+                <?php include '../templates/eventTree.php';?>
+            </div>
+            <div class="col-10">
+                <h3 class="text-center"><strong>Setting Up Device</strong></h3>
+                <div style="background-color:#FFFFFF; color:black;" class="col-8 container fluid-col">
+                    Hey
+                </div>
+            </div>
+            <div class="col-2" style="background-color:#202020">
+                <?php include '../templates/search.php';?>
+            </div>
+        </div>
+        <?php include '../templates/footer.php';?>
+    </div>
+</body>
+</html>
