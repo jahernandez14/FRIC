40512bae27590477756baa012c1f7d48907ff312
jahernandez14 <jahernandez14@miners.utep.edu>
2020-10-14 11:08:42 -0600

    added log functions to event and system controller

--- controller/subtaskController.php
+++ controller/subtaskController.php
@@ -1,5 +1,6 @@
 <?php
     require_once('/xampp/htdocs/FRIC/model/database.php');
+    require_once('/xampp/htdocs/FRIC/controller/logController.php');
 
     function subtaskOverviewTable(){
         $db = new Database("mongodb://localhost:27017");
