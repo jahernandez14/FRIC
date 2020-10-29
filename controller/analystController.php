97b2bf19bb4547dafa7aeee29ff7920d8fd8f7b5
Julio A Hernandez <jahernandez14@miners.utep.edu>
2020-10-29 00:35:51 -0600

    analyst progress methods created

--- controller/analystController.php
+++ controller/analystController.php
@@ -7,4 +7,24 @@
         $analystArray = $db->getAllAnalystNames();
         return $analystArray;
     }
+
+    function analystTaskSummary($fName, $lName){
+        $db = new AnalystDatabase();
+        return getAllProgressForTask($fName, $lName);
+    }
+
+    function analystSubTaskSummary($fName, $lName){
+        $db = new AnalystDatabase();
+        return getAllProgressForSubTask($fName, $lName);
+    }
+    
+    function analystFindingSummary($fName, $lName){
+        $db = new AnalystDatabase();
+        return getAllProgressForFinding($fName, $lName);
+    } 
+
+    function analystSystemSummary($fName, $lName){
+        $db = new AnalystDatabase();
+        return getAllProgressForSystem($fName, $lName);
+    } 
 ?>
