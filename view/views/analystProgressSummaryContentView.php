adf01e8970d615c31546c4a847a0ae671db51e6b
Julio A Hernandez <jahernandez14@miners.utep.edu>
2020-10-29 13:08:01 -0600

    setup title edited

--- view/views/analystProgressSummaryContentView.php
+++ view/views/analystProgressSummaryContentView.php
@@ -19,7 +19,7 @@
                 if (array_key_exists ("analystName", $_POST)){
                     $analystFirstName = explode(" ", $_POST["analystName"])[0];
                     $analystLastName = explode(" ", $_POST["analystName"])[1];
-                    echo "<h2><strong>" . $analystFirstName . " " . $analystLastName ."</strong></h2><br>";
+                    echo "<h3><strong>" . $analystFirstName . " " . $analystLastName ."</strong></h3><br>";
                 }
                 
                 ?>
