2fa66d00af242a442bc1730caa9f2af96321eba9
jamacas10 <43226243+jamacas10@users.noreply.github.com>
2020-10-29 04:04:57 -0600

    Created method in analystDB to calculate system progress for an analyst.
    Created method in analystDB to calculate system progress for an analyst.

--- model/testerClass.php
+++ model/testerClass.php
@@ -20,14 +20,16 @@ class testerClass{
 
     public function testSystems(){
         $systemDB  = new SystemDatabase();
-        $s1        = new Systeme($systemDB, "System 1", "This is a test Description", [], [], [], [], "This is a test test plan", "Low", "Medium", "High", false, 10, 10, .83);
+        $s1        = new Systeme($systemDB, "System 2", "This is a test Description", [], [], [], [], "This is a test test plan", "Low", "Medium", "High", false, 10, 10, .83);
+        $s1        = new Systeme($systemDB, "System 3", "This is a test Description", [], [], [], [], "This is a test test plan", "Low", "Medium", "High", false, 10, 10, .83);
+        $s1        = new Systeme($systemDB, "System 4", "This is a test Description", [], [], [], [], "This is a test test plan", "Low", "Medium", "High", false, 10, 10, .83);
     }
 
     public function testTasks(){
         $taskDB    = new TaskDatabase();
         $t1        = new Task($taskDB, "Task 1", "System 1", "This is test description", "High", "in progress", "11/01/2020", "Need to test this attribute", [], ["Will Lemon" , "Julio Hernandez"], [], false, 10, 10);
-        $t2        = new Task($taskDB, "Task 1", "System 1", "This is test description", "High", "in progress", "11/01/2020", "Need to test this attribute", [], ["Will Lemon"], [], false, 10, 10);
-        $t3        = new Task($taskDB, "Task 1", "System 1", "This is test description", "High", "in progress", "11/01/2020", "Need to test this attribute", [], ["Will Lemon", "Julio Hernandez"], [], false, 10, 10);
+        $t2        = new Task($taskDB, "Task 2", "System 1", "This is test description", "High", "in progress", "11/01/2020", "Need to test this attribute", [], ["Will Lemon"], [], false, 10, 10);
+        $t3        = new Task($taskDB, "Task 3", "System 3", "This is test description", "High", "in progress", "11/01/2020", "Need to test this attribute", [], ["Will Lemon", "Julio Hernandez"], [], false, 10, 10);
         
         //$analystDB = new AnalystDatabase();
         //print_r($analystDB->getAllProgressForTask("Will", "Lemon"));
@@ -36,28 +38,25 @@ class testerClass{
     public function testSubtasks(){
         $subtaskDB = new SubtaskDatabase();
         $st1       = new Subtask($subtaskDB,"Subtask 1", "Task 1", "This is test description", "not started", "11/01/2020", "Need to test this attribute", [] ,["Julio Hernandez"], [], false, 10);
-        $st2       = new Subtask($subtaskDB,"Subtask 1", "Task 1", "This is test description", "not started", "11/01/2020", "Need to test this attribute", [] , ["jeb"], [], false, 10);
-        $st3       = new Subtask($subtaskDB,"Subtask 1", "Task 1", "This is test description", "not started", "11/01/2020", "Need to test this attribute", [], ["Julio Hernandez","jeb"], [], false, 10);
+        $st2       = new Subtask($subtaskDB,"Subtask 2", "Task 3", "This is test description", "not started", "11/01/2020", "Need to test this attribute", [] , ["Jeb Mac"], [], false, 10);
+        $st3       = new Subtask($subtaskDB,"Subtask 3", "Task 1", "This is test description", "not started", "11/01/2020", "Need to test this attribute", [], ["Julio Hernandez","Jeb Mac"], [], false, 10);
     }
 
     public function testFindings(){
         $findingDB = new FindingDatabase();
-        $f1        = new Finding($findingDB,"Finding 1", "Jerble", "80", "Task 1", "", "", "This is a test description", "This is a test long description", "Open", "Creds", "Information", [], "Im testing this attribute", false, [], "low", "low", "high", ["Will Lemon", "JJ"], "Nearsider", "This is a test brief description", "This is a test long description", "Confirmed", "Very High", "This is a test impact description", "VH", "I", "We need to calculate SC", "We need to calculate VS", "We need to calculate QVS", "We need to calculate risk", "We need to calculate likelihood", "We need to calculate c impact on system", "We need to calculate i impact on system", "We need to calculate a impact on system", "We need to calculate impact score");
-        $f2        = new Finding($findingDB,"Finding 1", "Jerble", "80", "Task 1", "", "", "This is a test description", "This is a test long description", "Open", "Creds", "Information", [], "Im testing this attribute", false, [], "low", "low", "high", ["Will Lemon","Julio Hernandez"], "Nearsider", "This is a test brief description", "This is a test long description", "Confirmed", "Very High", "This is a test impact description", "VH", "I", "We need to calculate SC", "We need to calculate VS", "We need to calculate QVS", "We need to calculate risk", "We need to calculate likelihood", "We need to calculate c impact on system", "We need to calculate i impact on system", "We need to calculate a impact on system", "We need to calculate impact score");
-        $f3        = new Finding($findingDB,"Finding 1", "Jerble", "80", "Task 1", "", "", "This is a test description", "This is a test long description", "Open", "Creds", "Information", [], "Im testing this attribute", false, [], "low", "low", "high", ["Will Lemon", "Julio Hernandez"], "Nearsider", "This is a test brief description", "This is a test long description", "Confirmed", "Very High", "This is a test impact description", "VH", "I", "We need to calculate SC", "We need to calculate VS", "We need to calculate QVS", "We need to calculate risk", "We need to calculate likelihood", "We need to calculate c impact on system", "We need to calculate i impact on system", "We need to calculate a impact on system", "We need to calculate impact score");
+        $f1        = new Finding($findingDB,"Finding 1", "Billy Nye", "80", "Task 1", "", "", "This is a test description", "This is a test long description", "Open", "Creds", "Information", [], "Im testing this attribute", false, [], "low", "low", "high", ["Will Lemon", "JJ"], "Nearsider", "This is a test brief description", "This is a test long description", "Confirmed", "Very High", "This is a test impact description", "VH", "I", "We need to calculate SC", "We need to calculate VS", "We need to calculate QVS", "We need to calculate risk", "We need to calculate likelihood", "We need to calculate c impact on system", "We need to calculate i impact on system", "We need to calculate a impact on system", "We need to calculate impact score");
+        $f2        = new Finding($findingDB,"Finding 2", "Xerxes", "80", "Task 1", "", "", "This is a test description", "This is a test long description", "Open", "Creds", "Information", [], "Im testing this attribute", false, [], "low", "low", "high", ["Will Lemon","Julio Hernandez"], "Nearsider", "This is a test brief description", "This is a test long description", "Confirmed", "Very High", "This is a test impact description", "VH", "I", "We need to calculate SC", "We need to calculate VS", "We need to calculate QVS", "We need to calculate risk", "We need to calculate likelihood", "We need to calculate c impact on system", "We need to calculate i impact on system", "We need to calculate a impact on system", "We need to calculate impact score");
+        $f3        = new Finding($findingDB,"Finding 3", "Dan Bilzerian", "80", "Task 1", "", "", "This is a test description", "This is a test long description", "Open", "Creds", "Information", [], "Im testing this attribute", false, [], "low", "low", "high", ["Will Lemon", "Julio Hernandez"], "Nearsider", "This is a test brief description", "This is a test long description", "Confirmed", "Very High", "This is a test impact description", "VH", "I", "We need to calculate SC", "We need to calculate VS", "We need to calculate QVS", "We need to calculate risk", "We need to calculate likelihood", "We need to calculate c impact on system", "We need to calculate i impact on system", "We need to calculate a impact on system", "We need to calculate impact score");
         
         $analystDB = new AnalystDatabase();
-        // print_r($analystDB->getAllProgressForFinding("Will", "Lemon"));
     }
 
     public function testAnalysts(){
         $analystDB = new AnalystDatabase();
-        $a1        = new Analyst($analystDB,"Tim", "Honks", "TH", "123.12.111.1", "Forrest Gumpy", "Lead Role");
-        // print_r($analystDB->getAllAnalystForAssociation());
-    }
-
-    public function testStoringFile(){
-        echo "none";
+        $a1        = new Analyst($analystDB, "Tim", "Honks", "TH", "123.12.111.1", "Forrest Gumpy", "Lead Role");
+        $a1        = new Analyst($analystDB, "Julio", "Hernandez", "TH", "123.12.111.1", "Forrest Gumpy", "Lead Role");
+        $a1        = new Analyst($analystDB, "Will", "Lemon", "TH", "123.12.111.1", "Forrest Gumpy", "Lead Role");
+        $a1        = new Analyst($analystDB, "Jeb", "Mac", "TH", "123.12.111.1", "Forrest Gumpy", "Lead Role");
     }
 }
 ?>
