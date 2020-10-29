7a46ea3b7275a32369633ba4cb8fc3a36c9a4af2
Julio A Hernandez <jahernandez14@miners.utep.edu>
2020-10-29 14:11:44 -0600

    task overview post edited

--- view/views/taskOverview.php
+++ view/views/taskOverview.php
@@ -17,158 +17,17 @@
                 include '/xampp/htdocs/FRIC/controller/taskController.php';
 
                 if($_SERVER['QUERY_STRING'] == "postnew") {
-                    createSystem($_POST["systemName"], $_POST["systemDescription"], $_POST["systemLocation"], $_POST["systemRouter"], $_POST["systemSwitch"], $_POST["systemRoom"], $_POST["testPlan"], $_POST["confidentiality"], $_POST["integrity"], $_POST["availability"], 0, 0, "In Progress");
                     createTask($_POST["taskTitle"], $_POST["associatedSystem"], $_POST["taskDescription"], $_POST["taskPriority"], $_POST["taskProgress"], $_POST["taskDueDate"], $_POST["attachment"], $_POST["associationToTask"], $_POST["analystAssignment"], $_POST["collaboratorAssignment"], $_POST["archiveStatus"], $_POST["numberOfSubtasks"], $_POST["numberOfFindings"]);
                 }
 
                 if($_SERVER['QUERY_STRING'] == "postedit") {
-                    editSystem($_POST["systemID"], $_POST["systemName"], $_POST["systemDescription"], $_POST["systemLocation"], $_POST["systemRouter"], $_POST["systemSwitch"], $_POST["systemRoom"], $_POST["testPlan"], $_POST["confidentiality"], $_POST["integrity"], $_POST["availability"], FALSE, $_POST["numberOfTasks"], $_POST["numberOfFindings"], $_POST["progress"]);
+                    editTask($_POST["taskID"], $_POST["taskTitle"], $_POST["associatedSystem"], $_POST["taskDescription"], $_POST["taskPriority"], $_POST["taskProgress"], $_POST["taskDueDate"], $_POST["attachment"], $_POST["associationToTask"], $_POST["analystAssignment"], $_POST["collaboratorAssignment"], $_POST["archiveStatus"], $_POST["numberOfSubtasks"], $_POST["numberOfFindings"]);
                 }
 
                 $taskTable = table::tableByType("Task Overview", taskOverviewTable());
                 $taskTable->printTable();
                 ?>
 
-
-
-
-
-
-
-
-
-
-                <h2 class="text-center">Task Overview</h2>
-                <a href="../views/helpContentView.php" class="btn-sm btn-light" style=color:black>+</a>
-                <br></br>
-                <table class="table table-light table-striped">
-                    <thead>
-                        <tr>
-                            <th scope="col"><input type="checkbox"></th>
-                            <th scope="col">Title&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">System&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">Analyst&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">Priority&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">Progress&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">No. of Subtask&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">No. of Findings&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                            <th scope="col">Due Date&nbsp;
-                                <div class="btn-group-vertical">
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&uarr;</button>
-                                    <button class="btn btn-sm btn-secondary py-0"
-                                        style="font-size: .6em;">&darr;</button>
-                                </div>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th scope="col"><input type="checkbox"></th>
-                            <td>Task 1</td>
-                            <td>System 3</td>
-                            <td>am.123.1.123.2</td>
-                            <td>High</td>
-                            <td>In Progress</td>
-                            <td>0</td>
-                            <td>3</td>
-                            <td>9/12/2020</td>
-                        </tr>
-                        <tr>
-                            <th scope="col"><input type="checkbox"></th>
-                            <td>Task 1</td>
-                            <td>System 3</td>
-                            <td>am.123.1.123.2</td>
-                            <td>High</td>
-                            <td>In Progress</td>
-                            <td>0</td>
-                            <td>3</td>
-                            <td>9/12/2020</td>
-                        </tr>
-                        <tr>
-                            <th scope="col"><input type="checkbox"></th>
-                            <td>Task 1</td>
-                            <td>System 3</td>
-                            <td>am.123.1.123.2</td>
-                            <td>High</td>
-                            <td>In Progress</td>
-                            <td>0</td>
-                            <td>3</td>
-                            <td>9/12/2020</td>
-                        </tr>
-                        <tr>
-                            <th scope="col"><input type="checkbox"></th>
-                            <td>Task 1</td>
-                            <td>System 3</td>
-                            <td>am.123.1.123.2</td>
-                            <td>High</td>
-                            <td>In Progress</td>
-                            <td>0</td>
-                            <td>3</td>
-                            <td>9/12/2020</td>
-                        </tr>
-                        <tr>
-                            <th scope="col"><input type="checkbox"></th>
-                            <td>Task 1</td>
-                            <td>System 3</td>
-                            <td>am.123.1.123.2</td>
-                            <td>High</td>
-                            <td>In Progress</td>
-                            <td>0</td>
-                            <td>3</td>
-                            <td>9/12/2020</td>
-                        </tr>
-                    </tbody>
-                </table>
             </div>
             <div class="col-2" style="background-color:#202020">
                 <?php include '../templates/search.php';?>
