2e31c71367ff1cf127308f426a6affcb31ff97d4
WilliamBasquez <webasquez@outlook.com>
2020-11-12 09:52:55 -0700

    testing for Julio

--- model/erbReport.php
+++ model/erbReport.php
@@ -18,19 +18,65 @@
     $alignLeft = Alignment::HORIZONTAL_LEFT;
     $alignRight = Alignment::HORIZONTAL_RIGHT;
 
+    $objPHPPowerPoint = new PhpPresentation();
+
     function createERBreport($findingsArray){
         //Array with findings information
-        $objPHPPowerPoint = new PhpPresentation();
 
         //Slides Properties?
         $oMasterSlide = $objPHPPowerPoint->getAllMasterSlides()[0];
         $oSlideLayout = $oMasterSlide->getAllSlideLayouts()[0];
+
+        //Create first slide
+        $currentSlide = createSlide();
+
+        //Adding little disclaimer in the upper/lower-center of the slide
+        addDisclaimer($currentSlide, $alignCenter, $mainFont, $colorDGray);
+
+        //MainTitle
+        addText($currentSlide, 780, 115, 33, 250, $alignLeft, 'U.S. ARMY COMBAT CAPABILITIES DEVELOPMENT COMMAND —', false, $mainFont, 30, $colorBlack);
+
+        //Additional title
+        addText($currentSlide, 780, 80, 33, 350, $alignLeft, 'DATA & ANALYSIS CENTER', false, $mainFont, 30, $colorBlack);
+
+        //CVPA title
+        addText($currentSlide, 780, 80, 33, 460, $alignLeft, '[ CVPA Title ]', false, $mainFont, 20, $colorBlack);
+
+        //Presenter info
+        addText($currentSlide, 780, 30, 33, 565, $alignLeft, 'Name of Presenter', false, $mainFont, 12, $colorBlack);
+        addText($currentSlide, 780, 30, 33, 595, $alignLeft, 'Rank/Title of Presenter (Ex. CISSP, CELH, Security+)', false, $mainFont, 12, $colorBlack);
+        addText($currentSlide, 780, 30, 33, 627, $alignLeft, 'Cyber Experimentation & Analysis Division', false, $mainFont, 12, $colorBlack);
+
+        //Adding today's date
+        addText($currentSlide, 780, 30, 33, 682, $alignLeft, date("d m Y"), false, $mainFont, 8, $colorBlack);
+
+        //Adding all images in first slide
+        addImage($currentSlide, '../../view/images/army2.png', 'ARMY logo', 0, 170, 50, 50);
+        addImage($currentSlide, '../../view/images/cead2.png', 'CEAD logo', 0, 120, 210, 60);
+        addImage($currentSlide, '../../view/images/devcom.png', 'DEVCOM logo', 0, 105, 585, 60);
+
+        //Add a new slide to the presentation
+        $slide2 = $objPHPPowerPoint->createSlide();
+        $slide2->setSlideLayout($oSlideLayout);
+
+        //Adding little disclaimer in the upper/lower-center of the slide
+        addDisclaimer($slide2, $alignCenter, $mainFont, $colorDGray);
+
+        //Add small images
+        addImage($slide2, '../../view/images/army2.png', 'ARMY logo', 0, 85, 30, 30);
+        addImage($slide2, '../../view/images/cead2.png', 'CEAD logo', 0, 60, 120, 34);
+        addImage($slide2, '../../view/images/devcom.png', 'DEVCOM logo', 0, 53, 768, 30);
+    
+        $oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
+        $oWriterPPTX->save("../../model/ERBreport/ERBreport.pptx");
     }
 
     function createSlide(){
         // Create slide
         $currentSlide = $objPHPPowerPoint->getActiveSlide();
         $currentSlide->setSlideLayout($oSlideLayout);
+
+        return $currentSlide;
     }
 
     function addText($slide, $w, $h, $xOffset, $yOffset, $alignment, $text, $bold, $fontName, $fontSize, $fontColor){
@@ -64,7 +110,7 @@
                   ->setOffsetX($xOffset)
                   ->setOffsetY($yOffset);
     }
-
+/*
     //Adding little disclaimer in the upper/lower-center of the slide
     addDisclaimer($currentSlide, $alignCenter, $mainFont, $colorDGray);
 
@@ -111,8 +157,7 @@
     $textRun1->getFont()->setBold(true)
                         ->setSize(12)
                         ->setColor( $colorBlack );
-    */
     // mkdir('../../model/ERBreport');
     $oWriterPPTX = IOFactory::createWriter($objPHPPowerPoint, 'PowerPoint2007');
-    $oWriterPPTX->save("../../model/ERBreport/ERBreport.pptx");
+    $oWriterPPTX->save("../../model/ERBreport/ERBreport.pptx");*/
 ?>
