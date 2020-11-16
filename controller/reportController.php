c01f329568742eb450dc4683fd98c32c5948049e
WilliamBasquez <webasquez@miners.utep.edu>
2020-11-15 22:07:58 -0700

    Added a function to call in order to create a ERB report given findings and systems in an event

--- controller/reportController.php
+++ controller/reportController.php
@@ -3,12 +3,17 @@
     require_once('../../model/erbReport.php');
     require_once('../../model/riskMatrixReport.php');
 
+    require_once('findingController.php');
+    require_once('systemController.php');
+
     function readFinalReport($list){
         new FinalReport($list);
     }
 
-    function readERBReport($list){
-        createERB($list);
+    function createERBReport($list){
+        $findings = ERBFindings($list);
+        $systemTitles = readSystemTitles();
+        createERB($findings, $systemTitles);
     }
 
     function readMatrixReport($list){
