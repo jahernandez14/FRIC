86eff9b5ffcbdf6981875808ed99ee26c35b85b0
jamacas10 <43226243+jamacas10@users.noreply.github.com>
2020-11-18 20:33:35 -0700

    Changed progress scaling
    Changed progress scaling

--- model/analystDatabase.php
+++ model/analystDatabase.php
@@ -315,9 +315,9 @@ class AnalystDatabase extends Database{
 
     public function getAllProgressForSystem($analystFirstName, $analystLastName){
         try{
-            $query       = new MongoDB\Driver\Query([]);
-            $cursor      = $this->manager->executeQuery('FRIC_Database.Task', $query);
-            $totalTask   = array();
+            $query    = new MongoDB\Driver\Query([]);
+            $cursor   = $this->manager->executeQuery('FRIC_Database.Task', $query);
+            $totalTask= array();
             $totalCal = array();
             foreach($cursor as $document){
                 foreach($document->analystAssignment as $assignedAnalyst){
@@ -340,7 +340,7 @@ class AnalystDatabase extends Database{
                 $cursor = $this->manager->executeQuery('FRIC_Database.System', $query);
                 foreach($cursor as $document){
                     $row = array();
-                    array_push($row, $document->_id, $document->systemName, $document->numberOfTasks, $document->numberOfFindings, round((100/ (3 * $value)) * $totalCal[$key]) . "%");
+                    array_push($row, $document->_id, $document->systemName, $document->numberOfTasks, $document->numberOfFindings, round((100/ (10 * $value)) * $totalCal[$key]) . "%");
                     array_push($table, $row);
                 }
             }
@@ -381,9 +381,9 @@ class AnalystDatabase extends Database{
             case "transferred":
                 return 1;
             case "in progress":
-                return 2;
+                return 5;
             case "complete":
-                return 3;
+                return 10;
             default:
                 return 0;        
         }
