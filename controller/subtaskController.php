cee4ffc9c84ab0a7784ee5292da5ef6ee658a4fc
Daniel O'Brien <daobrien@miners.utep.edu>
2020-11-30 23:00:19 -0700

    Added restore

--- controller/subtaskController.php
+++ controller/subtaskController.php
@@ -45,4 +45,12 @@
                               $attr[9],$attr[10],true,$attr[12],$attr[13]);
         logEntry($attr[1] . " has been archived");
     }
+
+    function restoreSubTask($id){
+        $db = new subTaskDatabase();
+        $attr = $db->getSubTaskAttributes($id);
+        $db->editSubTaskDocument($attr[0],$attr[1],$attr[2],$attr[3],$attr[4],$attr[5],$attr[6],$attr[7],$attr[8],
+                              $attr[9],$attr[10],false,$attr[12],$attr[13]);
+        logEntry($attr[1] . " has been restored");
+    }
 ?>
